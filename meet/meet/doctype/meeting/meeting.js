637c1a13dbd20ba60956e011ea66cfdc3ec4e025
MatoloJr <muokiemmanuel@zetech.ac.ke>
2026-02-26 16:53:33 +0300

    integrated frappe framework functional methods for both python and javascript for the first time

--- meet/meet/doctype/meeting/meeting.js
+++ meet/meet/doctype/meeting/meeting.js
@@ -5,4 +5,23 @@
 // 	refresh(frm) {
 
 // 	},
-// });
+// }); 
+frappe.ui.form.on("Meeting", {
+    attendee: function(frm, cdt, cdn) {
+        var attendee = frappe.model.get_doc(cdt, cdn);
+        if (attendee.attendee) {
+            //if attendee is selected, fetch the full name and set it in the full name field
+            frm.call({
+                method: "meet.meet.doctype.meeting.meeting.get_full_name",
+                args: {
+                    attendee: attendee.attendee
+                },
+                callback: function(r) {
+                    frappe.model.set_value(cdt, cdn, "full_name", r.message);
+                }
+            });
+        } else {
+            //if no attendee is selected, clear the full name field
+            frappe.model.set_value(cdt, cdn, "full_name", null);
+        }
+    }})
--- meet/meet/doctype/meeting/meeting.py
+++ meet/meet/doctype/meeting/meeting.py
@@ -1,6 +1,11 @@
 # Copyright (c) 2026, emm and contributors
 # For license information, please see license.txt
 
+#other imports
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+
 # import frappe
 from frappe.model.document import Document
 
@@ -29,3 +34,23 @@ class meeting(Document):
 	# end: auto-generated types
 
 	pass
+
+#frappe methods and other custom methods
+def validate(self):
+	"""set missing name and warn if duplicate"""
+	found = []
+	for attendee in self.attendee:
+		if not attendee.full_name:
+			attendee.full_name = get_full_name(attendee.attendee)
+
+		if attendee.attendee in found:
+			frappe.throw(_("Attendee {0} entered twice").format(attendee.attendee))
+
+		found.append(attendee.attendee)
+
+#marked this function as a whitelist to be used in client side
+@frappe.whitelist()
+def get_full_name(attendee):
+	user = frappe.get_doc("User", attendee)
+	#concatenates by space if it has value
+	return " ".join(filter(None, [user.first_name, user.middle_name, user.last_name]))
